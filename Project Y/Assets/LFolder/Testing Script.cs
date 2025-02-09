b4db006e4fb431501c17f4cb35e00c75885eb08f
Leo-Cawley <23632687@stu.mmu.ac.uk>
2025-02-09 16:28:40 +0000

    Test 1 Success, Test 2 Code p1

--- Project Y/Assets/LFolder/Testing Script.cs
+++ Project Y/Assets/LFolder/Testing Script.cs
@@ -10,17 +10,38 @@ public class TestingScript : MonoBehaviour
     float fire2;
     float fire3;
 
+    
+    bool Light;
+    float LiDecCounter;
+
+    bool Heavy;
+    float HeDecCounter;
+
+    float basicCountMax;
 
     // Start is called before the first frame update
     void Start()
     {
+        Combo = false;
+        Light = false;
+        Heavy = false;
+
         
+        LiDecCounter = 0;
+        HeDecCounter = 0;
+
+        basicCountMax = 50;
     }
 
     // Update is called once per frame
     void Update()
     {
         Inputs();
+
+        HeavyDecay();
+        LightDecay();
+
+        ComboTracker();
     }
 
     void FixedUpdate()
@@ -51,21 +72,70 @@ public class TestingScript : MonoBehaviour
     void LightAttack()
     {
         cool.CoolnessIncrease(10);
-        
+
+        if(!Light)
+        {
+            Light = true;
+            LiDecCounter = basicCountMax;
+        }
+        else
+        {
+            LiDecCounter = basicCountMax;
+        }
+
+    }
+
+    void LightDecay()
+    {
+        if (LiDecCounter > 0)
+        {
+            LiDecCounter--;
+        }
+        else if (LiDecCounter == 0)
+        {
+            Light = false;
+        }
     }
 
     void HeavyAttack()
     {
         cool.CoolnessIncrease(25);
+        
+        if (!Heavy)
+        {
+            Heavy = true;
+            HEDecCounter = basicCountMax;
+        }
+        else
+        {
+            HeDecCounter = basicCountMax;
+        }
+    }
+
+    void HeavyDecay()
+    {
+        if (HeDecCounter > 0)
+        {
+            HeDecCounter--;
+        }
+        else if (HeDecCounter == 0)
+        {
+            Heavy = false;
+        }
     }
 
     void Gun()
     {
         cool.CoolnessIncrease(30);
+        cool.AddGunMult();
     }
 
     void ComboTracker()
     {
-
+        if(Heavy && Light)
+        {
+            cool.AddComboMult();
+        }
     }
+
 }

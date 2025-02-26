4a54d714252754665d7014daca33dfb911461b31
Kit <23651079@stu.mmu.ac.uk>
2025-02-26 15:54:03 +0000

    Changes

--- Project Y/Assets/CreepKitScripts/ItemEffects.cs
+++ Project Y/Assets/CreepKitScripts/ItemEffects.cs
@@ -23,22 +23,22 @@ public class ItemEffects : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D coll)
     {
-        if (coll.gameObject.tag == "Bigger")
-        {
-            rigidBody.transform.localScale = new Vector3(2, 2, 2);
-            Destroy(coll.gameObject);
-        }
+        //if (coll.gameObject.tag == "Bigger")
+     //   {
+        //    rigidBody.transform.localScale = new Vector3(2, 2, 2);
+       //     Destroy(coll.gameObject);
+       // }
 
-        if (coll.gameObject.tag == "Faster")
-        {
-            speed = 10.0f;
-            Destroy(coll.gameObject);
-        }
+       // if (coll.gameObject.tag == "Faster")
+       // {
+       //     speed = 10.0f;
+      //      Destroy(coll.gameObject);
+      //  }
 
-        if (coll.gameObject.tag == "Double")
-        {
+      //  if (coll.gameObject.tag == "Double")
+      //  {
 
-        }
+       // }
 
 
     }

b7f346cad1cf6663dc60f8efaddcf93e98ca5e26
Kit <23651079@stu.mmu.ac.uk>
2025-03-05 11:09:13 +0000

    Double is coded (not tested)

--- Project Y/Assets/CreepKitScripts/ItemEffects.cs
+++ Project Y/Assets/CreepKitScripts/ItemEffects.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,34 @@ public class ItemEffects : MonoBehaviour
 {
     Rigidbody2D rigidBody;
     public float speed = 5.0f;
-    public float jumpForce = 8.0f;
-    public float airControlForce = 10.0f;
-    public float airControlMax = 1.5f;
+    //public float jumpForce = 8.0f;
+    //public float airControlForce = 10.0f;
+    //public float airControlMax = 1.5f;
 
-    bool IsFire;
-    bool IsIce;
+    float timer = 0f;
+    
+    int HeavyAttackDamage = 10;
+    int LightAttackDamage = 5;
+    int RangedDamage;
 
-    //public GameObject BulletPrefab;
-    //public Transform firePoint;
+    bool IsWeaponFists;
+    bool IsWeaponKnuckles;
+    bool IsWeaponKnife;
+    bool IsWeaponBat;
+    bool IsWeaponCrowbar;
+
+    bool IsWeaponRevolver;
+    bool IsWeaponColt;
+    bool IsWeaponTommy;
+    bool IsWeaponShotGun;
+    bool IsWeaponFlamethrower;
+
+
+
+
+
+    private bool IsFire;
+    private bool IsIce;
     // Use this for initialization
     void Start()
     {
@@ -29,32 +49,47 @@ public class ItemEffects : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D coll)
     {
-        //if (coll.gameObject.tag == "Bigger")
-        //   { 
-        //    rigidBody.transform.localScale = new Vector3(2, 2, 2);
-        //     Destroy(coll.gameObject);
-        // }
+        if (coll.gameObject.tag == "Bigger")
+        {
+            Destroy(coll.gameObject);
+            timer += Time.deltaTime;
+            if (timer > 5f)
+            {
+                rigidBody.transform.localScale = new Vector3(2, 2, 2);
+            }
+        }
 
-        // if (coll.gameObject.tag == "Faster")
-        // {
-        //     speed = 10.0f;
-        //      Destroy(coll.gameObject);
-        //  }
+        if (coll.gameObject.tag == "Faster")
+        {
+            speed = 10.0f;
+        }
 
-        //  if (coll.gameObject.tag == "Double")
-        //  {
 
-        // }
+        //Bullet is shot out twice from the player
+        //This needs to last for a bit of time but not too much time (3 seconds?)
+        //In the case of melee, it needs to register twice
 
-        //  if (coll.gameObject.tag == "Piercing")
-        //  {
+        if (coll.gameObject.tag == "Double")
+        {
+            Destroy(coll.gameObject);
+            timer += Time.deltaTime;
+            if (timer > 3f)
+            {
+                HeavyAttackDamage = HeavyAttackDamage * 2;
+                Debug.Log(HeavyAttackDamage);
+            }  
+        }
 
-        // }
+        if(coll.gameObject.tag == "Piercing")
+        {
 
-        //  if (coll.gameObject.tag == "CoolMeterDependent")
-        //  {
+        }
 
-        // }
+        if(coll.gameObject.tag == "CoolMeterDependent")
+        { 
+
+        }
+
 
 
     }
